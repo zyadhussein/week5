bda4bb4278b31ebf7f5989a12340c5ff7ddf5355
zyadhussein <134060144+zyadhussein@users.noreply.github.com>
2023-07-05 20:26:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+''' author Zyad Hussein, learned from Data Glacier app
+created: 03/07/2023
+app is deployed on web by heroku
+'''
+
 import numpy as np
 from flask import Flask, request,render_template
 import pickle
@@ -11,16 +16,14 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+   
+    int_features = [int(x) for x in request.form.values()]#request the input variables needed for model
+    final_features = [np.array(int_features)]#take inputs and create numpy array ut of it
+    prediction = model.predict(final_features)#predictions generated from linear regression model trained
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0], 2)# output of the prediction rounded
 
-    return render_template('index.html', prediction_text='House price should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='House price should be $ {}'.format(output))#display the output to user
 
 if __name__ == "__main__":
     app.run(debug=True)
